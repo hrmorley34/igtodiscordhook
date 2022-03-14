ed6baf4b622fcc8915aa8e80cd0b2843490943e7
hrmorley34 <henry@morley.org.uk>
2022-03-14 16:12:36 +0000

    Send all posts in chronological order

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from dotenv import load_dotenv
+import instagrapi.types
 import json
 from os import environ
 from pathlib import Path
 from sqlmodel import create_engine
 
-from igtodiscordhook.ighook import IGHookManager
+from igtodiscordhook.ighook import IGHook, IGHookManager
 
 
 if __name__ == "__main__":
@@ -22,10 +23,24 @@ if __name__ == "__main__":
     manager = IGHookManager(engine)
     manager.login_ig(**CREDENTIALS, settings_file=SETTINGS_FILE)
 
+    all_unsents: list[tuple[IGHook, instagrapi.types.Media]] = []
     for accopts in OPTIONS["accounts"]:
         USER_USERNAME = accopts["igaccount"]
         USER_ID = manager.username_to_id(USER_USERNAME)
         ighook = manager.get_hook(USER_ID, accopts["webhook"])
-        ighook.update_hook()
+
+        with ighook.db.session() as session:
+            posts = list(ighook.get_all_posts(session))
+            unsents = ighook.filter_unsent_posts(session, posts)
+            session.commit()
+
+        for post in unsents:
+            all_unsents.append((ighook, post))
+
+        ighook.delete_missing_posts(posts)
+
+    all_unsents.sort(key=lambda pair: pair[1].taken_at)
+    for ighook, post in all_unsents:
+        ighook.push_post(post)
 
     manager.dump_settings(settings_file=SETTINGS_FILE)
